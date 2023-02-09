6d776cbbafaa638a0e10b15dc6ff8f7dea388b9e
Tarcisio <tarcisio.dovs@gmail.com>
2023-02-09 18:29:02 -0300

    requisito 5

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../compomemts/Header';
 
 export default class Game extends Component {
   render() {
     return (
-      <div>game</div>
+      <div>
+        <Header />
+        game
+      </div>
     );
   }
 }
