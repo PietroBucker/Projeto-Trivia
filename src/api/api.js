e587b09dbfbdbd9a7018cc2e6c7d95858f0cb4cb
Pietro Bucker <pietro_177@hotmail.com>
2023-02-09 18:12:18 -0300

    requisito 2

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+const requestApiTrivia = async () => {
+  const response = await fetch('https://opentdb.com/api_token.php?command=request');
+  const data = await response.json();
+  return data;
+};
+export default requestApiTrivia;
