70418104c313d7ac2df051902322e0ae07593773
max fukui <max.fukui.dev@gmail.com>
2023-02-13 15:49:16 -0400

    lint fixed

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
@@ -23,6 +24,16 @@ class Feedback extends Component {
     );
   }
 }
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+};
+
+Feedback.defaultProps = {
+  assertions: 0,
+  score: 0,
+};
 
 const mapStateToProps = (globalState) => ({
   ...globalState,
