4145c1a8169dfb0a38133c32868755bd9739a22e
Pietro Bucker <pietro_177@hotmail.com>
2023-02-13 19:30:09 -0300

    pull

---
+++ src/tests/helpers/requisito17.test.js
@@ -0,0 +1,10 @@
+import renderWithRouterAndRedux from './renderWithRouterAndRedux';
+import App from '../../App';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('', () => {
+  it('', () => {
+    
+  })
+})
