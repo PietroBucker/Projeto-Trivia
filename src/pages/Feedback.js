f94503498e9a92f19693df2171586e1dea203605
Tarcisio <tarcisio.dovs@gmail.com>
2023-02-13 16:56:36 -0300

    req-11

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,22 +4,9 @@ import { connect } from 'react-redux';
 class Feedback extends Component {
   render() {
     // const { assertions, score } = this.props;
-    const asserts = 3;
+
     return (
-      <div>
-        { assertions >= asserts
-          ? <span data-testid="feedback-text">Well Done!</span>
-          : <span data-testid="feedback-text">Could be better...</span> }
-        <span data-testid="feedback-total-score">
-          Sua pontuação foi:
-          { score }
-        </span>
-        <span
-          data-testid="feedback-total-score"
-        >
-          {/* {`Voce acertou ${assertions} preguntas`} */}
-        </span>
-      </div>
+      <div>a</div>
     );
   }
 }
